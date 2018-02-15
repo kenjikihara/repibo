fec584c23085f97870ea7bcb0c94f98df8a45441
Kenji Kihara <kenjikihara1220@gmail.com>
2018-02-15 08:45:32 +0000

    sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,17 +91,17 @@ Rails.application.configure do
   
   
   
-  # config.action_mailer.default_url_options = { host: 'repibo.herokuapp.com' }
-  # ActionMailer::Base.delivery_method = :smtp
-  # ActionMailer::Base.smtp_settings =
-  # {
-  # user_name: ENV['SENDGRID_USERNAME'],
-  # password: ENV['SENDGRID_PASSWORD'],
-  # domain: "heroku.com",
-  # address: "smtp.sendgrid.net",
-  # port: 587,
-  # authentication: :plain,
-  # enable_starttls_auto: true
-  # }
+  config.action_mailer.default_url_options = { host: 'repibo.herokuapp.com' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings =
+  {
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: "heroku.com",
+  address: "smtp.sendgrid.net",
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+  }
   
 end
