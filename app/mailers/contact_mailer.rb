fa10a0acf3da90eca974a5813db37fbb2da2f060
Kenji Kihara <kenjikihara1220@gmail.com>
2018-02-20 00:47:02 +0000

    contact mail to edit

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
 
-    mail to:  contact.email,
+    mail to:  're.picture.books@gmail.com',
     subject: "Re:piboへのお問い合わせメール"
   end
 end
