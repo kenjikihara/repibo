2f88f4bde48ad63ae18ee146aead9680f70ed40b
Kenji Kihara <kenjikihara1220@gmail.com>
2018-02-20 00:22:55 +0000

    contact mail

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
 
-    mail to: contact.email,
+    mail to:  contact.email,
     subject: "Re:piboへのお問い合わせメール"
   end
 end
