6d21dff42ba24df76974150e9878443f9e932c30
Kenji Kihara <kenjikihara1220@gmail.com>
2018-02-24 03:51:44 +0000

    いらないファイル削除,コーディング整頓

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  resources :messages, only: [:new, :create, :show]
-  resources :contacts, only: [:new, :create,]
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy, :edit]
-  resources :donations, only: [:index]
-  resources :disclaimers, only: [:index]
-  get 'tags/:tag', to: 'books#index', as: :tag
   resources :books do
     collection do
       post :confirm
     end
   end
-    
+  resources :contacts, only: [:new, :create,]
+  resources :disclaimers, only: [:index]
+  resources :donations, only: [:index]
+  resources :messages, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy, :edit]
+  resources :users
+  get 'tags/:tag', to: 'books#index', as: :tag
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
