5d617ce260aea4bbe49d6af8c27385d7b5d91eb0
Kenji Kihara <kenjikihara1220@gmail.com>
2018-03-05 21:34:22 +0000

    画像アップロードボタン修正、メッセージname,emailのvalue修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -24,24 +24,6 @@ class MessagesController < ApplicationController
       end
     end
   end
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to @message, notice: 'Message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-  def destroy
-    @message.destroy
-    respond_to do |format|
-      format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     def set_message
