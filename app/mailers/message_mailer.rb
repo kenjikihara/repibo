74fb0b622edd41d208b0199706a3db18b5f1711b
Kenji Kihara <kenjikihara1220@gmail.com>
2018-03-19 01:08:21 +0000

    不要な空行の削除 fix #16

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,7 +1,6 @@
 class MessageMailer < ApplicationMailer
   def message_mail(message)
     @message = message
-    
     mail to: message.book.user.email,
     subject: "投稿した絵本へのメッセージが届きました"
   end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ApplicationMailer
   def user_mail(user)
     @user = user
-
     mail to: user.email , subject: "Re:piboへの登録が完了しました"
   end
 end
