de93325c7bc94921b943ab9959c31e1297c56d60
Kenji Kihara <kenjikihara1220@gmail.com>
2018-05-28 07:36:51 +0000

    class直下にhelperを記述修正 fix #26

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
   protect_from_forgery with: :exception
   before_action :set_locale
   
@@ -10,5 +11,4 @@ class ApplicationController < ActionController::Base
     { locale: I18n.locale }.merge options
   end
     
-  include SessionsHelper
 end
