06430ee37ab2ce37afea6eb456bfd91c19324163
Kenji Kihara <kenjikihara1220@gmail.com>
2018-05-29 04:56:44 +0000

    include をclass直下記載に修正 fix #29

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,4 +1,6 @@
 module BooksHelper
+  include ActsAsTaggableOn::TagsHelper
+  
   def choose_new_or_edit
     if action_name == 'new' || action_name == 'confirm'
       confirm_books_path
@@ -6,6 +8,4 @@ module BooksHelper
       book_path
     end
   end
-  
-  include ActsAsTaggableOn::TagsHelper
 end
