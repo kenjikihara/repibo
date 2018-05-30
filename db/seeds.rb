3956d2db58b31857212582be6ba4e8b1a38cbf6d
Kenji Kihara <kenjikihara1220@gmail.com>
2018-05-30 07:34:46 +0000

    seedデータ作成 fix #32

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+10.times do |n|
+  name = Faker::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
